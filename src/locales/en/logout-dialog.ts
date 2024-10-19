d72acfacaa25e823a1dfeb863d18b1575e5cadf9
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-10-19 13:56:37 +0300

    fix: prettier

--- src/locales/en/logout-dialog.ts
+++ src/locales/en/logout-dialog.ts
@@ -2,8 +2,8 @@ export const logoutDialog = {
   accessibilityDescription:
     'Notification asking the user to choose between logging out or staying in the app',
   accessibilityTitle: 'Logout confirmation dialog',
-  visibleTitle: 'Logout',
   confirmButton: 'Yes',
-  visibleBodyText: 'Are you really want to log out of your account',
   rejectButton: 'No',
+  visibleBodyText: 'Are you really want to log out of your account',
+  visibleTitle: 'Logout',
 }
