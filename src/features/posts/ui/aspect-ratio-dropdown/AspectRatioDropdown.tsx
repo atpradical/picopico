3a7eea9860bdd6f90ef0579e15fb829e1c6ee0c3
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-11-05 22:31:52 +0300

    fix(dependencies): update picopico-ui-kit to 0.21.2
    Upgraded @atpradical/picopico-ui-kit in package.json and pnpm-lock.yaml from version 0.19.2 to 0.21.2. This ensures compatibility with recent changes and fixes within the package.
    
    feat(aspect-ratio-dropdown): add aspect ratio dropdown component
    
    Implemented the AspectRatioDropdown component for selecting aspect ratios. Added new styling in AspectRatioDropdown.module.scss and integrated it into the CropBody component.
    
    refactor(upload-button): enable upload button in CropBody
    
    Reactivate the file upload button in the CropBody component and renamed for clarity. This reestablishes the upload functionality with improved UI integration.

---
+++ src/features/posts/ui/aspect-ratio-dropdown/AspectRatioDropdown.tsx
@@ -0,0 +1,51 @@
+import {
+  AspectHorizontalRectangle,
+  AspectSquareRatioIcon,
+  AspectVerticalRectangle,
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  ExpandOutlineIcon,
+  ImageOutlineIcon,
+  Typography,
+} from '@atpradical/picopico-ui-kit'
+
+import s from './AspectRatioDropdown.module.scss'
+
+export const AspectRatioDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className={s.trigger} variant={'icon'}>
+          <ExpandOutlineIcon className={s.icon} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent align={'start'} className={s.dropdownContent} side={'top'}>
+          <DropdownMenuGroup>
+            <DropdownMenuItem className={s.item}>
+              <Typography>Original</Typography>
+              <ImageOutlineIcon className={s.icon} />
+            </DropdownMenuItem>
+            <DropdownMenuItem className={s.item}>
+              <Typography>1:1</Typography>
+              <AspectSquareRatioIcon className={s.icon} />
+            </DropdownMenuItem>
+            <DropdownMenuItem className={s.item}>
+              <Typography>4:5</Typography>
+              <AspectVerticalRectangle className={s.icon} />
+            </DropdownMenuItem>
+            <DropdownMenuItem className={s.item}>
+              <Typography>16:9</Typography>
+              <AspectHorizontalRectangle className={s.icon} />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
+  )
+}
--- src/features/posts/ui/create-post-dialog/dialog-bodies/CropBody.tsx
+++ src/features/posts/ui/create-post-dialog/dialog-bodies/CropBody.tsx
@@ -1,8 +1,9 @@
 import { ChangeEvent, ComponentPropsWithoutRef } from 'react'
 
-import { useTranslation } from '@/shared/hooks'
+import { POSTS_ALLOWED_UPLOAD_TYPES } from '@/features/posts/config'
+import { AspectRatioDropdown } from '@/features/posts/ui/aspect-ratio-dropdown'
 import { Nullable } from '@/shared/types'
-import { Carousel, DialogBody } from '@atpradical/picopico-ui-kit'
+import { Button, Carousel, DialogBody } from '@atpradical/picopico-ui-kit'
 import clsx from 'clsx'
 
 import s from '@/features/posts/ui/create-post-dialog/dialog-bodies/dialog.bodies.module.scss'
@@ -13,32 +14,28 @@ type CropBodyProps = {
 } & ComponentPropsWithoutRef<typeof DialogBody>
 
 export const CropBody = ({ onUpload, previewList, ...props }: CropBodyProps) => {
-  // const {
-  //   t: { createPostDialog },
-  // } = useTranslation()
-
   return (
-    <>
-      <DialogBody className={clsx(s.body, s.withPreview)} {...props}>
-        <div className={s.previewSizes}>
-          <Carousel slides={previewList ?? []} />
-          {/*  <Image*/}
-          {/*    alt={createPostDialog.altDescription}*/}
-          {/*    className={s.image} // todo: определиться с cover или contain*/}
-          {/*    fill*/}
-          {/*    src={previewList[0]}*/}
-          {/*  />*/}
-        </div>
-      </DialogBody>
-      {/*<Button as={'label'} className={s.button} variant={'primary'}>*/}
-      {/*  <input*/}
-      {/*    accept={POSTS_ALLOWED_UPLOAD_TYPES.join(', ')}*/}
-      {/*    hidden*/}
-      {/*    onChange={onUpload}*/}
-      {/*    type={'file'}*/}
-      {/*  />*/}
-      {/*  {createPostDialog.buttons.selectFilesButton}*/}
-      {/*</Button>*/}
-    </>
+    <DialogBody className={clsx(s.body, s.withPreview)} {...props}>
+      <div className={s.previewSizes}>
+        <Carousel slides={previewList ?? []} />
+        <AspectRatioDropdown />
+
+        {/*  <Image*/}
+        {/*    alt={createPostDialog.altDescription}*/}
+        {/*    className={s.image} // todo: определиться с cover или contain*/}
+        {/*    fill*/}
+        {/*    src={previewList[0]}*/}
+        {/*  />*/}
+        <Button as={'label'} className={s.button} variant={'primary'}>
+          <input
+            accept={POSTS_ALLOWED_UPLOAD_TYPES.join(', ')}
+            hidden
+            onChange={onUpload}
+            type={'file'}
+          />
+          Upload one more photo
+        </Button>
+      </div>
+    </DialogBody>
   )
 }
