fd36a3af915cb1c5e1e8918fca408d0306bcdf8f
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-11-06 12:44:48 +0300

    chore: remove AspectRatioDropdown feature
    Removed the AspectRatioDropdown component and its SCSS module. Updated package dependencies to the latest version and adjusted CropBody to reflect these changes.

--- src/features/posts/ui/create-post-dialog/dialog-bodies/CropBody.tsx
+++ src/features/posts/ui/create-post-dialog/dialog-bodies/CropBody.tsx
@@ -1,9 +1,7 @@
 import { ChangeEvent, ComponentPropsWithoutRef } from 'react'
 
-import { POSTS_ALLOWED_UPLOAD_TYPES } from '@/features/posts/config'
-import { AspectRatioDropdown } from '@/features/posts/ui/aspect-ratio-dropdown'
 import { Nullable } from '@/shared/types'
-import { Button, Carousel, DialogBody } from '@atpradical/picopico-ui-kit'
+import { Carousel, DialogBody } from '@atpradical/picopico-ui-kit'
 import clsx from 'clsx'
 
 import s from '@/features/posts/ui/create-post-dialog/dialog-bodies/dialog.bodies.module.scss'
@@ -18,23 +16,21 @@ export const CropBody = ({ onUpload, previewList, ...props }: CropBodyProps) => 
     <DialogBody className={clsx(s.body, s.withPreview)} {...props}>
       <div className={s.previewSizes}>
         <Carousel slides={previewList ?? []} />
-        <AspectRatioDropdown />
-
         {/*  <Image*/}
         {/*    alt={createPostDialog.altDescription}*/}
         {/*    className={s.image} // todo: определиться с cover или contain*/}
         {/*    fill*/}
         {/*    src={previewList[0]}*/}
         {/*  />*/}
-        <Button as={'label'} className={s.button} variant={'primary'}>
-          <input
-            accept={POSTS_ALLOWED_UPLOAD_TYPES.join(', ')}
-            hidden
-            onChange={onUpload}
-            type={'file'}
-          />
-          Upload one more photo
-        </Button>
+        {/*<Button as={'label'} className={s.button} variant={'primary'}>*/}
+        {/*  <input*/}
+        {/*    accept={POSTS_ALLOWED_UPLOAD_TYPES.join(', ')}*/}
+        {/*    hidden*/}
+        {/*    onChange={onUpload}*/}
+        {/*    type={'file'}*/}
+        {/*  />*/}
+        {/*  Upload one more photo*/}
+        {/*</Button>*/}
       </div>
     </DialogBody>
   )
