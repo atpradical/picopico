92443c744200ce19cd247fb57f40d36799bccbd6
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-11-07 19:44:30 +0300

    chore: update @atpradical/picopico-ui-kit to v0.23.0
    Updated the `@atpradical/picopico-ui-kit` dependency version in `package.json` and `pnpm-lock.yaml` to 0.23.0. This includes changes to several components in `CropBody.tsx` and updates to associated styles.
    
    ### feat: enhance post creation dialog with new UI tools
    
    Integrated additional UI tools in `CropBody.tsx` for better image manipulation in the post creation dialog. Added buttons, sliders, popovers, and toggle groups for different image ratio options. Adjusted related styles in `dialog.bodies.module.scss` for these new components and overlays.

--- src/features/posts/ui/create-post-dialog/dialog-bodies/CropBody.tsx
+++ src/features/posts/ui/create-post-dialog/dialog-bodies/CropBody.tsx
@@ -1,8 +1,27 @@
 import { ChangeEvent, ComponentPropsWithoutRef } from 'react'
 
 import { Nullable } from '@/shared/types'
-import { Carousel, DialogBody } from '@atpradical/picopico-ui-kit'
+import {
+  Button,
+  Carousel,
+  CloseOutlineIcon,
+  DialogBody,
+  ExpandOutlineIcon,
+  HorizontalRectangleIcon,
+  ImageOutlineIcon,
+  MaximizeOutlineIcon,
+  PlusCircleOutlineIcon,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Slider,
+  SquareIcon,
+  ToggleGroup,
+  ToggleGroupItem,
+  VerticalRectangleIcon,
+} from '@atpradical/picopico-ui-kit'
 import clsx from 'clsx'
+import Image from 'next/image'
 
 import s from '@/features/posts/ui/create-post-dialog/dialog-bodies/dialog.bodies.module.scss'
 
@@ -16,21 +35,71 @@ export const CropBody = ({ onUpload, previewList, ...props }: CropBodyProps) => 
     <DialogBody className={clsx(s.body, s.withPreview)} {...props}>
       <div className={s.previewSizes}>
         <Carousel slides={previewList ?? []} />
-        {/*  <Image*/}
-        {/*    alt={createPostDialog.altDescription}*/}
-        {/*    className={s.image} // todo: определиться с cover или contain*/}
-        {/*    fill*/}
-        {/*    src={previewList[0]}*/}
-        {/*  />*/}
-        {/*<Button as={'label'} className={s.button} variant={'primary'}>*/}
-        {/*  <input*/}
-        {/*    accept={POSTS_ALLOWED_UPLOAD_TYPES.join(', ')}*/}
-        {/*    hidden*/}
-        {/*    onChange={onUpload}*/}
-        {/*    type={'file'}*/}
-        {/*  />*/}
-        {/*  Upload one more photo*/}
-        {/*</Button>*/}
+        <div className={s.toolsContainer}>
+          <div className={s.leftToolsContainer}>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button className={s.buttonTrigger} variant={'icon'}>
+                  <ExpandOutlineIcon className={s.iconImage} />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent align={'start'} className={s.popoverContent} side={'top'}>
+                <ToggleGroup className={s.toggleGroup} defaultValue={'original'} type={'single'}>
+                  <ToggleGroupItem className={s.toggleItem} value={'original'}>
+                    Original <ImageOutlineIcon className={s.iconImage} />
+                  </ToggleGroupItem>
+                  <ToggleGroupItem className={s.toggleItem} value={'1:1'}>
+                    1:1 <SquareIcon className={s.iconRatio} />
+                  </ToggleGroupItem>
+                  <ToggleGroupItem className={s.toggleItem} value={'4:5'}>
+                    4:5 <VerticalRectangleIcon className={s.iconRatio} />
+                  </ToggleGroupItem>
+                  <ToggleGroupItem className={s.toggleItem} value={'9:16'}>
+                    9:16 <HorizontalRectangleIcon className={s.iconRatio} />
+                  </ToggleGroupItem>
+                </ToggleGroup>
+              </PopoverContent>
+            </Popover>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button className={s.buttonTrigger} variant={'icon'}>
+                  <MaximizeOutlineIcon className={s.iconImage} />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent align={'start'} className={s.popoverSliderContent} side={'top'}>
+                <Slider className={s.slider} />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button className={s.buttonTrigger} variant={'icon'}>
+                <ImageOutlineIcon className={s.iconImage} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent align={'end'} className={s.popoverImagesContainer} side={'top'}>
+              <div className={s.uploadImagesContainer}>
+                <div className={s.addContainer}>
+                  <div className={s.prviewContainer}>
+                    <Image
+                      alt={'description'}
+                      className={s.image} // todo: определиться с cover или contain
+                      height={80}
+                      src={previewList?.[0] ?? ''}
+                      width={80}
+                    />
+                    <Button className={s.closeButton} variant={'icon'}>
+                      <CloseOutlineIcon />
+                    </Button>
+                  </div>
+                  <Button className={s.addButton} variant={'icon'}>
+                    <PlusCircleOutlineIcon className={s.plusIcon} />
+                  </Button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
     </DialogBody>
   )
