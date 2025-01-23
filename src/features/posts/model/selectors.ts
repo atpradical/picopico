cbb353381d1ad277d70cec9977225b13a9dfc2da
atpradical <ivan.dolgikh1199@gmail.com>
2025-01-23 17:53:08 +0300

    Remove unused selector and fix CSS formatting issues
    The `selectPostContent` selector was removed as it was no longer in use. Additionally, minor formatting adjustments were made to the `.actions` class in the ProfileHeader stylesheet for consistency.

--- src/features/posts/model/selectors.ts
+++ src/features/posts/model/selectors.ts
@@ -1,19 +1,4 @@
-import { selectPublicationsAllData } from '@/features/publication/model'
 import { AppState } from '@/lib/store'
-import { createSelector } from 'reselect'
 
 export const selectCreatePostAllData = (state: AppState) => state.createPost
 export const selectPostDialogMeta = (state: AppState) => state.createPost.dialogMeta
-
-// Селектор для получения контента поста по его postId
-export const selectPostContent = createSelector(
-  [selectPublicationsAllData, (state: AppState, postId: number) => postId],
-  (publications, postId) => {
-    // Убедитесь, что publications.posts существует и является массивом
-    if (Array.isArray(publications.posts)) {
-      return publications.posts.find(el => el.id === postId)
-    }
-
-    return undefined // Возвращаем undefined, если posts не существует или не является массивом
-  }
-)
