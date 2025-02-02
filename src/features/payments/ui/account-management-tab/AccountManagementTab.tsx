c2831251e1837dd6e18bcfd070857c4ed06ee6eb
atpradical <ivan.dolgikh1199@gmail.com>
2025-02-02 12:48:58 +0300

    Set AlertDialog isOpen prop to false by default
    Updated the isOpen prop of AlertDialog to false, ensuring it remains closed initially. This change prevents unintended display of the dialog and aligns with expected component behavior for proper user interaction.

--- src/features/payments/ui/account-management-tab/AccountManagementTab.tsx
+++ src/features/payments/ui/account-management-tab/AccountManagementTab.tsx
@@ -35,7 +35,7 @@ export const AccountManagementTab = ({ ...props }: AccountManagementTabProps) =>
         </Button>
       </div>
       <AlertDialog
-        isOpen
+        isOpen={false}
         onConfirm={() => {}}
         onOpenChange={() => {}}
         t={{
