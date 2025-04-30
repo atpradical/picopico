b2b86ccc541cc275aafe62b353bae4c1bbf4b844
atpradical <ivan.dolgikh1199@gmail.com>
2025-04-30 16:55:20 +0300

    Add infinite scroll and search functionality to SearchPage
    Implemented infinite scrolling with cursor-based pagination and integrated search functionality with debouncing. Updated UI to display results dynamically and show a loading spinner during data fetching. Refactored code for better modularity, introducing reusable hooks and user services.

--- src/views/search/ui/serch-result/SearchResult.tsx
+++ src/views/search/ui/serch-result/SearchResult.tsx
@@ -1,19 +1,32 @@
+import { ElementRef, forwardRef } from 'react'
+
+import { User } from '@/services/users'
+import { Paths } from '@/shared/enums'
 import { Avatar, Card, Typography } from '@atpradical/picopico-ui-kit'
 import Link from 'next/link'
 
 import s from './SearchResult.module.scss'
 
-type SearchResultProps = {}
-export const SearchResult = ({}: SearchResultProps) => {
-  return (
-    <Link href={'#'}>
-      <Card className={s.card}>
-        <Avatar size={'s'} src={'/favicon-dark-32x32.png?v=2'} />
-        <div>
-          <Typography>Ekat_Ivanova</Typography>
-          <Typography grey>Ekaterina Ivanova</Typography>
-        </div>
-      </Card>
-    </Link>
-  )
+type SearchResultProps = {
+  user: User
 }
+type SearchResultItemRef = ElementRef<'div'>
+
+export const SearchResult = forwardRef<SearchResultItemRef, SearchResultProps>((props, ref) => {
+  const { user } = props
+  const fullUserName = user.firstName ? `${user.firstName} ${user.lastName}` : 'Anonymous'
+
+  return (
+    <div className={s.container} ref={ref}>
+      <Link href={Paths.Profile + `/${user.id}`}>
+        <Card className={s.card}>
+          <Avatar size={'s'} src={user.avatars[1]?.url} />
+          <div>
+            <Typography className={s.userName}>{user.userName}</Typography>
+            <Typography grey>{fullUserName}</Typography>
+          </div>
+        </Card>
+      </Link>
+    </div>
+  )
+})
